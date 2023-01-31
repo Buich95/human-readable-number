d8e179562ea2d27f1e3b72c182c9f80af1b5b4fa
Buich95 <buyakov95@mail.ru>
2023-02-01 02:02:16 +0300

    feat: finish test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const a = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const b = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let result = "";
+
+    if (number.toString().length === 3) {
+        result += a[Math.trunc(number / 100)] + " hundred";
+        number -= Math.trunc(number / 100) * 100;
+        if (number === 0) {
+            return result;
+        }
+    }
+
+    if (number.toString().length === 2) {
+        if (number < 20) {
+            result += " " + a[number];
+            return result.trim();
+        } else {
+            result += " " + b[Math.trunc(number / 10) - 2];
+            number -= Math.trunc(number / 10) * 10;
+            if (number === 0) {
+                return result.trim();
+            }
+        }
+    }
+
+    if (number.toString().length === 1) {
+        result += " " + a[number];
+        return result.trim();
+    }
+};
